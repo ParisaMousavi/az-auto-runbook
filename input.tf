148f83bc61e4c05e70b7b4a9fe0d7f6ec224f3dd
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2023-02-18 12:08:15 +0100

    update

---
+++ input.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "automation_account_name" {
+  type = string
+}
+
+variable "log_verbose" {
+  type    = bool
+  default = true
+}
+
+variable "log_progress" {
+  type    = bool
+  default = true
+}
+
+variable "description" {
+  type = string
+}
+
+variable "runbook_type" {
+  type = string
+
+}
+
+variable "publish_content_link" {
+  type    = string
+  default = null
+}
---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_automation_runbook" "this" {
+  name                    = var.name
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  automation_account_name = var.automation_account_name
+  log_verbose             = var.log_verbose
+  log_progress            = var.log_progress
+  description             = var.description
+  runbook_type            = var.runbook_type
+  dynamic "publish_content_link" {
+    for_each = var.publish_content_link == null ? [] : [1]
+    content {
+      uri = var.publish_content_link
+    }
+  }
+}
